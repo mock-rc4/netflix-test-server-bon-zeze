a4bec7b06ba1d230f18115d3683744ad2d931de4
xfrnk2 <xfrnk2@gmail.com>
2022-03-20 00:27:37 +0900

    refacor: 불필요한 status 항목을 지우고 필요한 항목을 추가

--- src/main/java/com/example/demo/config/BaseResponseStatus.java
+++ src/main/java/com/example/demo/config/BaseResponseStatus.java
@@ -22,13 +22,17 @@ public enum BaseResponseStatus {
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
 
-    // users
-    USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
 
-    // [POST] /users
-    POST_USERS_EMPTY_EMAIL(false, 2015, "이메일을 입력해주세요."),
-    POST_USERS_INVALID_EMAIL(false, 2016, "이메일 형식을 확인해주세요."),
-    POST_USERS_EXISTS_EMAIL(false,2017,"중복된 이메일입니다."),
+    // [POST] /accounts
+    POST_ACCOUNTS_EMPTY_EMAIL(false, 2015, "이메일을 입력해주세요."),
+    POST_ACCOUNTS_INVALID_EMAIL(false, 2016, "이메일 형식을 확인해주세요."),
+    POST_ACCOUNTS_EXISTS_EMAIL(false,2017,"중복된 이메일입니다."),
+	POST_ACCOUNTS_EMPTY_PASSWORD(false,2018,"비밀번호를 입력해주세요."),
+	POST_ACCOUNTS_INVALID_PASSWORD(false,2019,"비밀번호 형식을 확인해주세요."),
+    POST_ACCOUNTS_INVALID_PHONE_NUMBER(false,2020,"휴대폰번호 형식을 확인해주세요."),
+    POST_ACCOUNTS_DEACTIVATED_ACCOUNT(false,2021,"회원탈퇴등의 이유로 비활성화된 유저입니다."),
+	POST_ACCOUNTS_EMPTY_MEMBERSHIP(false,2022,"멤버쉽을 입력해주세요."),
+
 
 
 
@@ -38,7 +42,7 @@ public enum BaseResponseStatus {
     // Common
     RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
 
-    // [POST] /users
+    // [POST] /accounts
     DUPLICATED_EMAIL(false, 3013, "중복된 이메일입니다."),
     FAILED_TO_LOGIN(false,3014,"없는 아이디거나 비밀번호가 틀렸습니다."),
 
@@ -50,7 +54,7 @@ public enum BaseResponseStatus {
     DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
     SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
 
-    //[PATCH] /users/{userIdx}
+    //[PATCH] /accounts/{accountIdx}
     MODIFY_FAIL_USERNAME(false,4014,"유저네임 수정 실패"),
 
     PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
