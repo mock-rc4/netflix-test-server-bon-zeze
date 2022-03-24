213ae9b0c7fa3f8778458a49d5b2648481f0e831
xfrnk2 <xfrnk2@gmail.com>
2022-03-24 17:02:15 +0900

    feat: 영화나 시리즈를 장르별로 조회하여 목록을 반환하는 API 기능 구현

--- src/main/java/com/example/demo/src/video/VideoController.java
+++ src/main/java/com/example/demo/src/video/VideoController.java
@@ -1,34 +1,50 @@
 package com.example.demo.src.video;
 
-import static com.example.demo.config.BaseResponseStatus.*;
-
 import java.util.List;
 
-import com.example.demo.config.BaseException;
-import com.example.demo.config.BaseResponse;
-import com.example.demo.src.profile.domain.Profile;
-import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.video.domain.Video;
+import com.example.demo.utils.JwtService;
 
 @RestController
 @RequestMapping("/videos")
 public class VideoController {
 
 	@Autowired
-	private final VideoService VideoService;
+	private final VideoService videoService;
 	@Autowired
-	private final VideoProvider VideoProvider;
+	private final VideoProvider videoProvider;
 	@Autowired
 	private final JwtService jwtService;
 
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	public VideoController(VideoService VideoService, VideoProvider VideoProvider, JwtService jwtService) {
-		this.VideoService = VideoService;
-		this.VideoProvider = VideoProvider;
+	public VideoController(VideoService videoService, VideoProvider videoProvider, JwtService jwtService) {
+		this.videoService = videoService;
+		this.videoProvider = videoProvider;
 		this.jwtService = jwtService;
 	}
+
+	@ResponseBody
+	@GetMapping("/genre")
+	public BaseResponse<List<Video.getVideoResDto>> getByGenre(@RequestParam int videoType,
+		@RequestParam String genre) {
+		try {
+			List<Video.getVideoResDto> resDto = videoProvider.getVideosByGenre(videoType, genre);
+			return new BaseResponse<>(resDto);
+		} catch (BaseException exception) {
+			return new BaseResponse<>((exception.getStatus()));
+		}
+	}
+
 }
--- src/main/java/com/example/demo/src/video/VideoDao.java
+++ src/main/java/com/example/demo/src/video/VideoDao.java
@@ -1,11 +1,12 @@
 package com.example.demo.src.video;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-
+import com.example.demo.src.video.domain.Video;
 
 @Repository
 public class VideoDao {
@@ -15,5 +16,26 @@ public class VideoDao {
 	@Autowired
 	public VideoDao(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public List<Video.getVideoResDto> getVideosByGenre(int videoType, String genre) {
+		String isMovieOrSeries = videoType == 0 ? "=" : "<";
+		String query = "select V.* from GenreContact as GC "
+			+ "left join Video as V on GC.videoIdx = V.videoIdx "
+			+ "left join Genre as G on G.genreIdx = GC.genreIdx where G.name = ? and 0 " + isMovieOrSeries
+			+ "V.season;";
+		return this.jdbcTemplate.query(query,
+			(rs, rowNum) -> new Video.getVideoResDto(
+				rs.getInt("videoIdx"),
+				rs.getInt("year"),
+				rs.getInt("season"),
+				rs.getInt("ageGrade"),
+				rs.getString("title"),
+				rs.getString("runningTime"),
+				rs.getString("photoUrl"),
+				rs.getString("summary"),
+				rs.getString("director")
+			),
+			genre);
 	}
 }
