9d499d3101f053863cd83eaee2c13a32d920d20d
yyy96 <dlduswo1247@gmail.com>
2022-03-25 15:39:15 +0900

    refactor: Top10 콘텐츠 조회 쿼리 변경

--- src/main/java/com/example/demo/src/video/VideoDao.java
+++ src/main/java/com/example/demo/src/video/VideoDao.java
@@ -57,8 +57,8 @@ public class VideoDao {
 
     //현재 10개까지 값이 안불러와지는 것은 좋아요에 대한 데이터가 부족해서. (데이터 추가 필요)
     public List<GetVideoRes> getTopTenVideos() {
-        String query = "select Video.videoIdx, photoUrl, ageGrade, season, runningTime, resolution\n" +
-                "from (select videoIdx, count(*) as likeCount from Assessment group by videoIdx order by likeCount desc limit 10) as Popular\n" +
+        String query = "select totalScore, Video.videoIdx, photoUrl, ageGrade, season, runningTime, resolution\n" +
+                "from (select videoIdx, sum(status) as totalScore from Assessment group by videoIdx order by totalScore desc limit 10) as Popular\n" +
                 "join Video on Video.videoIdx = Popular.videoIdx";
 
         List<GetVideoRes> videos = this.jdbcTemplate.query(query, videoRowMapper());
