388f58a2cfa00668521e8cbd6e2d575de2380982
yyy96 <dlduswo1247@gmail.com>
2022-03-26 18:54:33 +0900

    refactor: main categories uri 목록 조회 API 리팩토링

--- src/main/java/com/example/demo/src/category/domain/MainCategory.java
+++ src/main/java/com/example/demo/src/category/domain/MainCategory.java
@@ -3,6 +3,8 @@ package com.example.demo.src.category.domain;
 import com.example.demo.src.genre.domain.Genre;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -10,27 +12,17 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MainCategory {
 
-    private static final String TOP_TEN = "topten";
-    private static final String BOOKMARK = "bookmark";
-    private static final String POPULAR = "popular";
-    private static final String WATCHING = "watching";
+    public static final String TOP_TEN = "topten";
+    public static final String BOOKMARK = "bookmark";
+    public static final String POPULAR = "popular";
+    public static final String WATCHING = "watching";
 
-    private String topten_uri;
-    private String bookmark_uri;
-    private String popular_uri;
-    private String watching_uri;
-    private List<Genre> genres_uri;
+    private List<String> main_categories_uri;
+    private List<Genre> genres_categories_uri;
 
-
-    public MainCategory(List<Genre> GENRES) {
-        this(TOP_TEN, BOOKMARK, POPULAR, WATCHING, GENRES);
+    public MainCategory(List<Genre> genres) {
+        this.main_categories_uri = new ArrayList<>(Arrays.asList(TOP_TEN, BOOKMARK, POPULAR, WATCHING));
+        this.genres_categories_uri = genres;
     }
 
-    public MainCategory(String topten_uri, String bookmark_uri, String popular_uri, String watching_uri, List<Genre> genres_uri) {
-        this.topten_uri = topten_uri;
-        this.bookmark_uri = bookmark_uri;
-        this.popular_uri = popular_uri;
-        this.watching_uri = watching_uri;
-        this.genres_uri = genres_uri;
-    }
 }
