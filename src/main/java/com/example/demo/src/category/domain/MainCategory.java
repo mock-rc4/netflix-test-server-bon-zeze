fbc0591af5354f45f3b2623724577a10ff163725
yyy96 <dlduswo1247@gmail.com>
2022-03-29 17:18:44 +0900

    refactor: 메인 카테고리 파라미터 수정

--- src/main/java/com/example/demo/src/category/domain/MainCategory.java
+++ src/main/java/com/example/demo/src/category/domain/MainCategory.java
@@ -12,17 +12,17 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MainCategory {
 
-    public static final String TOP_TEN = "topten";
-    public static final String BOOKMARK = "bookmark";
-    public static final String POPULAR = "popular";
-    public static final String WATCHING = "watching";
+    public static final String TOP_TEN = "한국의 Top 10 콘텐츠";
+    public static final String BOOKMARK = "내가 찜한 콘텐츠";
+    public static final String POPULAR = "지금 뜨는 콘텐츠";
+    public static final String WATCHING = "시청중인 콘텐츠";
 
-    private List<String> main_categories_uri;
-    private List<Genre> genres_categories_uri;
+    private List<String> main_categories;
+    private List<Genre> genres_categories;
 
     public MainCategory(List<Genre> genres) {
-        this.main_categories_uri = new ArrayList<>(Arrays.asList(TOP_TEN, BOOKMARK, POPULAR, WATCHING));
-        this.genres_categories_uri = genres;
+        this.main_categories = new ArrayList<>(Arrays.asList(TOP_TEN, BOOKMARK, POPULAR, WATCHING));
+        this.genres_categories = genres;
     }
 
 }
